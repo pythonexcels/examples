8fc00df5543e9ad8a11cf53c56609ac4b978392e
dlwhite <dlwhite@synopsys.com>
2019-09-20 18:04:39 -0700

    Updated driving.py for Python 3

--- driving.py
+++ driving.py
@@ -8,7 +8,7 @@ wb = excel.Workbooks.Add()
 ws = wb.Worksheets('Sheet1')
 ws.Name = 'Built with Python'
 ws.Cells(1,1).Value = 'Hello Excel'
-print ws.Cells(1,1).Value
+print (ws.Cells(1,1).Value)
 for i in range(1,5):
     ws.Cells(2,i).Value = i  # Don't do this
 ws.Range(ws.Cells(3,1),ws.Cells(3,4)).Value = [5,6,7,8]
