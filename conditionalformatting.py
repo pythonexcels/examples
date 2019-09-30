ca33b475eeaafc482caaa28367e2ed7e6045cda5
dlwhite <dlwhite@synopsys.com>
2019-09-29 19:53:20 -0700

    Updated conditionalformatting.py for Python 3

--- conditionalformatting.py
+++ conditionalformatting.py
@@ -1,19 +1,20 @@
 #
 # conditionalformatting.py
-# Create two tables and apply Conditional Formatting
+# Create two tables and apply conditional formatting
 #
 import win32com.client as win32
 excel = win32.gencache.EnsureDispatch('Excel.Application')
-#excel.Visible = True
+# excel.Visible = True
 wb = excel.Workbooks.Add()
 ws = wb.Worksheets('Sheet1')
 ws.Range("B2:K2").Value = [i for i in range(1,11)]
-ws.Range("B2:B11").Value = zip([i for i in range(1,11)])
+ws.Range("B2:B11").Value = list(zip([i for i in range(1,11)]))
 ws.Range("C3").Formula = "=$B3*C$2"
 ws.Range("C3:C3").Select()
 excel.Selection.AutoFill(ws.Range("C3:K3"),win32.constants.xlFillDefault)
 ws.Range("C3:K3").Select()
 excel.Selection.AutoFill(ws.Range("C3:K11"),win32.constants.xlFillDefault)
+# Add the table of random integers
 ws.Range("B13:K22").Formula = "=INT(RAND()*100)"
 ws.Range("B2:K22").Select()
 excel.Selection.FormatConditions.AddColorScale(ColorScaleType = 3)
@@ -29,6 +30,7 @@ csc2.FormatColor.TintAndShade = 0
 csc3.Type = win32.constants.xlConditionValueHighestValue
 csc3.FormatColor.Color = 7039480
 csc3.FormatColor.TintAndShade = 0
+ws.Range("B:K").ColumnWidth = 4
 ws.Range("A1").Select()
 wb.SaveAs('ConditionalFormatting.xlsx')
 excel.Application.Quit()
